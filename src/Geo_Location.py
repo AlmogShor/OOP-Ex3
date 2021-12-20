63f262ebb36659cb85721e7740ededf72f412569
khsabrina <sabrina.khazanov@gmail.com>
2021-12-19 19:29:15 -0800

    TSP+Center+FromJson

---
+++ src/Geo_Location.py
@@ -0,0 +1,15 @@
+class Geo_Location:
+
+    def __init__(self, x,y,z):
+        self.x= x
+        self.y=y
+        self.z=z
+
+    def get_X(self):
+        return self.x
+
+    def get_Y(self):
+        return self.y
+
+    def get_Z(self):
+        return self.z
